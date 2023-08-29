618fdfc84a2e32bec6c191aea17a98422d97cf59
Abdoul <abdoulramanediallo44@gmail.com>
2023-08-29 15:35:54 +0000

    alter decoration && test files in actions

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -7,6 +9,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -18,4 +24,5 @@ class Person
   def of_age?
     @age.to_i >= 18
   end
+
 end
